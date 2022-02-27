5c9cdb7d33b89e1c9140b4518b6b811d56b41bb6
Maan17 <maansiverma8@gmail.com>
2022-02-27 21:01:38 +0530

    Create Post

---
+++ network/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class User(AbstractUser):
+    pass
+
+class post(models.Model):
+    owner = models.CharField(max_length=64, default="")
+    desc = models.TextField()
+    post_creation_date = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(null=True)
---
+++ network/views.py
@@ -0,0 +1,76 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
+
+from .forms import postForm
+from .models import User,post
+
+
+def index(request):
+    return render(request, "network/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+@csrf_protect
+def create_post(request):
+    form = postForm(request.POST, request.FILES)
+    if form.is_valid():
+        tweet = form.save(commit = False)
+        tweet.owner = request.user
+        tweet.save()
+        return index(request)
+    else:
+        return render(request, 'network/create_post.html', {'form': form})
